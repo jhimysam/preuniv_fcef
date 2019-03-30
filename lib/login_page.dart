f2b6f8f3a86cb1a46cf84c65d553af18cc58c32d
jhimysam <jhimysam@gmail.com>
2019-03-30 08:27:32 -0400

    inputText en login_page
    insertamos un inputText a nuestro formulario de login en el login_page.dart

---
+++ lib/login_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+//import 'package:login/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      
+      decoration: InputDecoration(
+        hintText: 'Ingrese su CI',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            email,
+            SizedBox(height: 8.0),
+            
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
