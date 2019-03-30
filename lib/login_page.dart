440d5aebd1677bfbbcaed36565c75edc5ff6b9d9
jhimysam <jhimysam@gmail.com>
2019-03-30 08:39:15 -0400

    logo en login_page
    Añadimos el logo en en nuestro formulario

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,7 +11,14 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     
-
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
     final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
@@ -32,6 +39,8 @@ class _LoginPageState extends State<LoginPage> {
           shrinkWrap: true,
           padding: EdgeInsets.only(left: 24.0, right: 24.0),
           children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
             email,
             SizedBox(height: 8.0),
             
