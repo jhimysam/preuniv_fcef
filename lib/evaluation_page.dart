22c11a74569099eda2d53c5c65b1e2a842ea2c47
jhimysam <jhimysam@gmail.com>
2019-04-10 11:07:07 -0400

    Update 'evaluation_page.dart'
    we prepare the environment for te navigation bottom

--- lib/evaluation_page.dart
+++ lib/evaluation_page.dart
@@ -5,26 +5,8 @@ class EvaluationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final welcome = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Welcome fcef rocks!!!!',
-        style: TextStyle(fontSize: 28.0, color: Colors.black87),
-      ),
-    );
-
-    
-
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      child: Column(
-        children: <Widget>[welcome],
-      ),
-    );
-
-    return Scaffold(
-      body: body,
+    return MaterialApp(
+      home: new EvalPage()
     );
   }
 }
